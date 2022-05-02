460d7b0d06013db0700574810a10f1d56a7e9cfc
Gr1mmz <admin@kraevskiy-web.ru>
2022-05-02 16:12:11 +0800

    add custom datepickers functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
 import '../scss/main.scss';
 import '../index.html';
 
+//ENTRY DATEPICKER
 
+const dateEntryInput = document.querySelector('#entryDate');
+const textEntryInput = document.querySelector('.datepicker__entry-date');
+dateEntryInput.addEventListener('change', event => {
+  textEntryInput.textContent = dateEntryInput.value.split('-').reverse().join('.');
+});
 
+//LEFT DATEPICKER
+
+const dateLeftInput = document.querySelector('#leftDate');
+const textLeftInput = document.querySelector('.datepicker__left-date');
+dateLeftInput.addEventListener('change', event => {
+  textLeftInput.textContent = dateLeftInput.value.split('-').reverse().join('.');
+});
 
 //CUSTOM SELECT
 
