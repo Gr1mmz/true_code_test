f8cde5c47a8ae88b0e0722e7c9508edf8440c459
Gr1mmz <admin@kraevskiy-web.ru>
2022-05-02 15:47:59 +0800

    add custom select

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,26 @@ import '../scss/main.scss';
 import '../index.html';
 
 
+
+
+//CUSTOM SELECT
+
+const customSelect = document.querySelector('.select');
+const customSelectWrapper = document.querySelector('.select__wrapper');
+const customSelectOptions = document.querySelectorAll(".select__option");
+
+customSelect.addEventListener('click', () => {
+  customSelectWrapper.classList.toggle('open');
+});
+
+for (const option of customSelectOptions) {
+  option.addEventListener('click', function() {
+    if (!this.classList.contains('selected')) {
+      this.parentNode.querySelector('.select__option.selected').classList.remove('selected');
+      this.classList.add('selected');
+      this.closest('.select').querySelector('.select__trigger .select__guests').textContent = this.textContent;
+    }
+  })
+}
+
+
